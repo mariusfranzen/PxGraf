cc44de58cc717482da911618aa541c4dee3a7628
Sakari Malkki <sakari.malkki@stat.fi>
2025-01-09 10:33:21 +0200

    Moves database table error to its own file

--- PxGraf/Models/Responses/DatabaseItems/DatabaseTable.cs
+++ PxGraf/Models/Responses/DatabaseItems/DatabaseTable.cs
@@ -6,14 +6,6 @@ using System;
 
 namespace PxGraf.Models.Responses.DatabaseItems
 {
-    public enum DatabaseTableError
-    {
-        Null,
-        ContentLoad,
-        ContentDimensionMissing,
-        TimeDimensionMissing,
-    }
-
     /// <summary>
     /// Represents a px database table.
     /// </summary>
